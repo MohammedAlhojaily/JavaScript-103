218f008cc2e4966536f730f55d854e0f1e939efb
MohammedAlhojaily <tu4201254@taibahu.edu.sa>
2023-11-18 17:22:24 +0300

    Default Destructuring

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
-let colors = ['red', 'green']
-let [firstColor, secondColor, thirdColor] = colors
+let colors = ['red', 'green', 'black']
+let [firstColor, secondColor, thirdColor = 'blue'] = colors
 console.log(firstColor, secondColor, thirdColor);
