bb7554248d77cfa668673fe9e89f75715360f7f4
MohammedAlhojaily <tu4201254@taibahu.edu.sa>
2023-11-18 17:20:01 +0300

    Destructuring

--- main.js
+++ main.js
@@ -1,30 +1,3 @@
-const newUser = new Promise((resolve, reject) =>{
-setTimeout(() =>{
-    const userInfo = {
-        name: 'Khalid',
-        age: 11
-    }
-    resolve(userInfo)
-}, 3000)
-})
-
-const addNewUser = userInfo => {
-    let userAge = userInfo.age
-    let adultAge;
-    if( userAge >= 18 ){
-        adultAge = `${userInfo.name} is an Adult`
-        return Promise.resolve(adultAge)
-    }else{
-        adultAge = `${userInfo.name} is not an Adult`
-        return Promise.reject(new Error(adultAge))
-    }
-}
-newUser
-    .then(addNewUser)
-    .then(res => {
-        console.log('** User info received **');
-        console.log(res);
-})
-    .catch(err =>{
-        console.log(err.message);
-    })
+let ages = [25, 30, 35]
+let [firstAge, secondAge, thirdAge] = ages
+console.log(firstAge, secondAge, thirdAge);
