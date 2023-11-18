419bd566a4a18954fc0d5a5dc40983e91ccf73db
MohammedAlhojaily <tu4201254@taibahu.edu.sa>
2023-11-18 16:24:54 +0300

    Text replacement

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-const str = `RegExr was created by gskinner.com, and is proudly hosted by Media Temple.
-Edit the Expression Text to see matches. Roll over matches or the expression for details. PCRE JavaScript flavors of RegEx are supported. Validate your expression with Tests mode.
-The side bar includes a Cheatsheet, full Reference, and Help. You can also Save & Share with the Community and view patterns you create or favorite in My Patterns.
-Explore results with the Tools below. Replace & List output custom results. Details lists capture groups. Explain describes your expression in plain English.`
-
-let myRegex = str.match(/and/g)
-console.log(myRegex.length);
+function replace() {
+let searchText = document.getElementById('search').value;
+let replaceText = document.getElementById('replace').value;
+let text = document.getElementById('text').innerHTML;
+let newText = document.getElementById('text').value;
+let reg = new RegExp(searchText, 'gi')
+newText.innerHTML = text
+}
